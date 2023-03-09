81124db834e6c7b5fcdbf3b00255b1a60e2abdb7
Nwamanna <nwamannazika@gmail.com>
2023-03-09 21:19:09 +0100

    python modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 import sys
-array = sys.argv
-i = 1
-if len(array) - 1 == 0:
-    print("0 argumnents.")
-else:
-    if len(array) - 1 == 1:
-        print("{} argument:".format(len(array) - 1))
+if __name__ == "__main__":
+    array = sys.argv
+    i = 1
+    if len(array) - 1 == 0:
+        print("0 arguments.")
     else:
-        print("{} arguments:".format(len(array) - 1))
-    while i < len(array):
-	    print('{:d}: {}'.format(i, array[i]))
-	    i += 1
+        if len(array) - 1 == 1:
+            print("{} argument:".format(len(array) - 1))
+        else:
+            print("{} arguments:".format(len(array) - 1))
+        while i < len(array):
+	        print('{:d}: {}'.format(i, array[i]))
+	        i += 1
