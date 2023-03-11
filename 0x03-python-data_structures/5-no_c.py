c8ba8218be01f249a2dae14fb33ea6125a3c82a7
Nwamanna <nwamannazika@gmail.com>
2023-03-11 15:11:28 +0100

    removes c and C

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    """ deletes c or C in a string """
+    new_string = ""
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            new_string += i
+    return new_string
+            
