23af61b38b4dc29f6904ac274d7b17b72a68a65f
Nwamanna <nwamannazika@gmail.com>
2023-03-15 14:45:17 +0100

    Square matrix element

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     """ returns the square value of all elements in a matrix """
-    if matrix == None:
+    if matrix is None:
         return
     if len(matrix) <= 0:
         return
     new_list = []
     for i in matrix:
-        new_list.append(list(map(lambda x: (x ** 2),i)))
+        new_list.append(list(map(lambda x: (x ** 2), i)))
     return new_list
