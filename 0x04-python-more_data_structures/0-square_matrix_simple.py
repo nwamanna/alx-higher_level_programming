125ce516326c03442563122d63d05c3df4c0fff3
Nwamanna <nwamannazika@gmail.com>
2023-03-15 14:33:28 +0100

    squares matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    """ returns the square value of all elements in a matrix """
+    if matrix == None:
+        return
+    if len(matrix) <= 1:
+        return
+    new_list = []
+    for i in matrix:
+        new_list.append(list(map(lambda x: (x ** 2),i)))
+    return new_list
