e014daee011c44f2b54f549ab347b2507b0f9c0d
Nwamanna <nwamannazika@gmail.com>
2023-03-15 14:43:46 +0100

    Update Readme

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,7 @@ def square_matrix_simple(matrix=[]):
     """ returns the square value of all elements in a matrix """
     if matrix == None:
         return
-    if len(matrix) <= 1:
+    if len(matrix) <= 0:
         return
     new_list = []
     for i in matrix:
