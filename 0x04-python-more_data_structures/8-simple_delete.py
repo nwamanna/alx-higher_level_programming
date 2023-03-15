a25d504c0ec181e752ce90ed262c4e286ee880dc
Nwamanna <nwamannazika@gmail.com>
2023-03-15 21:23:19 +0100

    delete a key

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/
+def simple_delete(a_dictionary, key=""):
+    """ deletes key in dictionary """
+    if type(key) != str:
+        return a_dictionary
+    if key not in a_dictionary:
+        return a_dictionary
+    a_dictionary.pop(key)
+    return a_dictionary
