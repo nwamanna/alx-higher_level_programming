7a07dae4753aeefd69b213601a3622d1d7cdbaa9
Nwamanna <nwamannazika@gmail.com>
2023-03-27 18:40:16 +0100

    handle all exceptions

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -15,6 +15,6 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
             print("out of range")
         finally:
-             new_list.append(result)
+            new_list.append(result)
         i += 1
     return (new_list)
