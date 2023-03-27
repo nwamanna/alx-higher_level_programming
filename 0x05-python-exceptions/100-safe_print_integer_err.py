1c591a1ec3fa112a486482d712a607363c5846cf
Nwamanna <nwamannazika@gmail.com>
2023-03-27 21:29:04 +0100

    send error message

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+    """ safely prints value even if it is not an integer """
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError as err:
+        sys.stderr.write(f'Exception: {err}\n')
+        return False
