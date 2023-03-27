a863d6828f79e6bcdb506ba1788cb8bc268c325f
Nwamanna <nwamannazika@gmail.com>
2023-03-27 17:52:58 +0100

    raise IndexError

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    """ prints out x number of elements in my_list """
+    i = 0
+    count = 0
+    while i < x:
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except IndexError:
+             raise
+        except ValueError:
+              pass
+        except TypeError:
+               pass
+        i += 1
+    print()
+    return (count)
