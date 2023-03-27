d580ebf1529f7f974039704c96bc21be76264303
Nwamanna <nwamannazika@gmail.com>
2023-03-27 18:35:24 +0100

    handle all exceptions

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+def list_division(my_list_1, my_list_2, list_length):
+    """ returns a new list """
+    i = 0
+    new_list = []
+    while i < list_length:
+        try:
+            result = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            result = 0
+            print("wrong type")
+        except ZeroDivisionError:
+            result = 0
+            print("division by zero")
+        except IndexError:
+            result = 0
+            print("out of range")
+        finally:
+             new_list.append(result)
+        i += 1
+    return (new_list)
