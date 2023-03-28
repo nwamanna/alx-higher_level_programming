c29a4d67cdb7a830d0344048e2a812fa6c9e6cd8
Nwamanna <nwamannazika@gmail.com>
2023-03-28 17:50:03 +0100

    private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/python3
+class Square:
+    """ This class defines a square.
+
+    The __init__ method instantiates the square with a size
+
+    Args:
+        size (int): size of square
+
+    Attributes:
+        size (int): size of square
+    """
+    def __init__(self, size):
+        self.__size = size
