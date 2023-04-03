1e15ec7bcbcc1844980833c6675cedd9b15a5b0a
Nwamanna <nwamannazika@gmail.com>
2023-04-03 21:32:10 +0100

    add del method

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+""" This module contains a class called Rectangle """
+
+
+class Rectangle:
+    """ Rectangle class that has all the properties of a rectangle """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        w = self.width
+        h = self.height
+        return w * h
+
+    def perimeter(self):
+        w = self.width
+        h = self.height
+        if w == 0 or h == 0:
+            p = 0
+        else:
+            p = 2 * (w + h)
+        return p
+
+    def __str__(self):
+        rec = []
+        w = self.width
+        h = self.height
+        if w == 0 or h == 0:
+            return ""
+        while h:
+            i = 0
+            while i < w:
+                rec.append("#")
+                i += 1
+            if h == 1:
+                break
+            rec.append("\n")
+            h -= 1
+        return "".join(rec)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.width}, {self.height})"
+
+    def __del__(self):
+        print("Bye rectangle...")
