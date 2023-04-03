62a6dfaee2f08c7521778dad9a549bb82047ec02
Nwamanna <nwamannazika@gmail.com>
2023-04-03 18:51:28 +0100

    rectangle class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" This module contains a class called Rectangle """
+
+
+class Rectangle:
+    """ Rectangle class that has all the properties of a rectangle """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,7 @@
+0x08-python-more_classes
+
+0. Simple rectangle
+mandatory
+Write an empty class Rectangle that defines a rectangle:
+
+You are not allowed to import any module
