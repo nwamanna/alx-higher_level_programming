f287d7b62178cab7ae2e09a6b9a4936b31828b2e
Nwamanna <nwamannazika@gmail.com>
2023-04-11 21:06:31 +0100

    magic method

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" This module contain a BaseGeometry class """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Empty class """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__height * self.__width
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
