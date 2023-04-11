9d7697fbe88e7b79d02d61d3fc79987051cbee38
Nwamanna <nwamannazika@gmail.com>
2023-04-11 18:51:53 +0100

    build sort feature

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Subclass of list """
+class MyList(list):
+    """ a subclass of list that can arrange in ascending order """
+    def print_sorted(self):
+        """ prints list in ascending order """
+        sort = sorted(self)
+        print(sort)
