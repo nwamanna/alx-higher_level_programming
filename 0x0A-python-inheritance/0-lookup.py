f9581f0b9f02d80b703baa5adb0267358b8747b8
Nwamanna <nwamannazika@gmail.com>
2023-04-11 18:58:05 +0100

    implement attribute feature

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ contains function that retrives attributes and methods of an object """
+
+
 def lookup(obj):
     """ returns a list of attributes and methods of an object """
     return dir(obj)
