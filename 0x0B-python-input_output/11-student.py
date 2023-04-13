a8c9c111c7f1a6483440d0bcc018d83a5824f759
Nwamanna <nwamannazika@gmail.com>
2023-04-13 13:38:56 +0100

    class with loading feature

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -14,8 +14,9 @@ class Student:
     def to_json(self, attrs=None):
         serial_dict = {}
         for key, value in self.__dict__.items():
-            if attrs != None:
-                if key in attrs and isinstance(value, (list, dict, str, int, bool)):
+            test = isinstance(value, (list, dict, str, int, bool))
+            if attrs is not None:
+                if key in attrs and test:
                     serial_dict[key] = value
             else:
                 if isinstance(value, (list, dict, str, int, bool)):
