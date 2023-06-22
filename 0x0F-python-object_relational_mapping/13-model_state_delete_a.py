10eca83d5545954595fe614a1cc0a9c39528cc95
Nwamanna <nwamannazika@gmail.com>
2023-06-22 10:57:49 +0100

    use pycodestyle

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -10,13 +10,13 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                       .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                       pool_pre_ping=True)
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
     delete_user = session.query(State).filter(
-                                              State.name.ilike('%a%')
+                                               State.name.ilike('%a%')
                                              ).order_by(State.id).all()
     for user in delete_user:
-       session.delete(user)
+        session.delete(user)
     session.commit()
