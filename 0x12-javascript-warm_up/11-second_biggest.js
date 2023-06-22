c4b720427b0e1a4663442390220fbe36a32f15bc
Nwamanna <nwamannazika@gmail.com>
2023-06-22 13:49:05 +0100

    fix fuction

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const args = process.argv.length;
-if (args === 3) {
+if (args <= 3) {
   console.log(0);
-} else if (args > 3) {
-  console.log('Arguments found');
 } else {
-  console.log(0);
+  const arry = process.argv.slice(2).map(Number);
+  const sArr = arry.sort(function (a, b) { return b - a; })[1];
+  console.log(sArr);
 }
